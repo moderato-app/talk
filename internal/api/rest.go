7b2dbb1f1ce30cf9d8eb9474b696e70402df58c8
Clement <gh.2lgqz@aleeas.com>
2023-08-25 14:17:29 +0800

    feat: SSE

--- internal/api/rest.go
+++ internal/api/rest.go
@@ -14,3 +14,32 @@ type AskResp struct {
 type TranscribeResp struct {
 	Text string `json:"text"`
 }
+
+const (
+	paramTypeRange = iota
+	paramTypeSingleChoice
+	paramTypeMultiChoice
+)
+
+// supported params by client
+// todo 发送的时候用这个类，接收的时候用结构化的类unmarshal
+type SupportedParamResp struct {
+	LLMParam []Param
+	STTParam []Param
+	TTSParam []Param
+}
+
+type Param struct {
+	Name string `json:"name"`
+	Type int
+
+	// range
+	RangeIsFloat bool   `json:"rangeIsFloat"`
+	RangeStart   string `json:"rangeStart"`
+	RangeEnd     string `json:"rangeEnd"`
+
+	// single choice
+	SChoices []string
+	// multi choice
+	MChoices []string
+}
