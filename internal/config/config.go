cf0922af8f1fc21750cedf83fd76e55a6d64a08f
Clement <gh.2lgqz@aleeas.com>
2023-08-30 17:47:19 +0800

    feat: LLM tunability

--- internal/config/config.go
+++ internal/config/config.go
@@ -47,24 +47,6 @@ type LlmConfig struct {
 	OpenAIChatGPT OpenAIChatGPTConfig `mapstructure:"open-ai-chat-gpt"`
 }
 
-// model for ChatGPT
-const (
-	GPT432K0613           = "gpt-4-32k-0613"
-	GPT432K0314           = "gpt-4-32k-0314"
-	GPT432K               = "gpt-4-32k"
-	GPT40613              = "gpt-4-0613"
-	GPT40314              = "gpt-4-0314"
-	GPT4                  = "gpt-4"
-	GPT3Dot5Turbo0613     = "gpt-3.5-turbo-0613"
-	GPT3Dot5Turbo0301     = "gpt-3.5-turbo-0301"
-	GPT3Dot5Turbo16K      = "gpt-3.5-turbo-16k"
-	GPT3Dot5Turbo16K0613  = "gpt-3.5-turbo-16k-0613"
-	GPT3Dot5Turbo         = "gpt-3.5-turbo"
-	GPT3Dot5TurboInstruct = "gpt-3.5-turbo-instruct"
-)
-
 type OpenAIChatGPTConfig struct {
-	APIKey             string `mapstructure:"api-key"`
-	Model              string `mapstructure:"model"`
-	MaxGenerationToken int    `mapstructure:"max-generation-token"`
+	APIKey string `mapstructure:"api-key"`
 }
---
+++ pkg/providers/chatgpt/defaults.go
@@ -0,0 +1,62 @@
+package chatgpt
+
+import (
+	"math"
+
+	"github.com/proxoar/talk/pkg/client/tune"
+)
+
+// DefaultChatGPTTunability
+// see https://platform.openai.com/docs/api-reference/chat/create
+var DefaultChatGPTTunability = tune.LLMTunability{
+	Model: tune.SingleChoice[string]{
+		Tunable: true,
+		Choices: []tune.Choice[string]{
+			{Value: "gpt-3.5-turbo"},
+			{Value: "gpt-3.5-turbo-0613"},
+			{Value: "gpt-3.5-turbo-0301"},
+			{Value: "gpt-3.5-turbo-16k"},
+			{Value: "gpt-3.5-turbo-16k-0613"},
+			{Value: "gpt-3.5-turbo-instruct"},
+			{Value: "gpt-4"},
+			{Value: "gpt-4-0613"},
+			{Value: "gpt-4-0314"},
+			{Value: "gpt-4-32k-0613"},
+			{Value: "gpt-4-32k-0314"},
+			{Value: "gpt-4-32k"},
+		},
+		Default: "gpt-3.5-turbo",
+	},
+	MaxTokens: tune.IntRange{
+		Tunable:    true,
+		RangeStart: 0,
+		RangeEnd:   math.MaxInt,
+		Default:    16,
+	},
+	Temperature: tune.FloatRange{
+		Tunable:    true,
+		RangeStart: 0,
+		RangeEnd:   2,
+		Default:    1,
+	},
+	PresencePenalty: tune.FloatRange{
+		Tunable:    true,
+		RangeStart: -2,
+		RangeEnd:   2,
+		Default:    0,
+	},
+	FrequencyPenalty: tune.FloatRange{
+		Tunable:    true,
+		RangeStart: -2,
+		RangeEnd:   2,
+		Default:    0,
+	},
+}
+
+var DefaultChatGPTTuneOption = tune.LLMTuneOption{
+	Model:            "gpt-3.5-turbo",
+	MaxTokens:        nil,
+	Temperature:      nil,
+	PresencePenalty:  nil,
+	FrequencyPenalty: nil,
+}
