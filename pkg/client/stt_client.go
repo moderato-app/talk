45c5d1b3a019743d628a39c2fa8a70b732b646f8
Clement <gh.2lgqz@aleeas.com>
2023-09-15 21:32:03 +0800

    feature: add eTag middleware to control cache of static files

---
+++ pkg/client/stt_client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"context"
+	"io"
+
+	"github.com/proxoar/talk/pkg/ability"
+)
+
+type Voice struct {
+	Id         string
+	Name       string
+	Lang       string
+	Accent     string
+	Gender     string
+	PreviewUrl string
+	Labels     map[string]string
+}
+
+type SpeechToText interface {
+	// SpeechToText transcribes voice into text
+	SpeechToText(ctx context.Context, audio io.Reader, fileName string, option ability.STTOption) (string, error)
+	SetAbility(ctx context.Context, a *ability.STTAblt) error
+	// Support
+	//
+	// read ability.STTOption to check if current provider support the option
+	Support(o ability.STTOption) bool
+}
