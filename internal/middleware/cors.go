3c2e5650b528ae7486df40d33fe73983a567956e
Clement <gh.2lgqz@aleeas.com>
2023-09-17 14:39:11 +0800

    feat: re-design drop-down-menu, add feature of downloading audio

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "github.com/labstack/echo/v4/middleware"
 
 var AllowAllCors = middleware.CORSWithConfig(middleware.CORSConfig{
 	AllowOrigins:  []string{"*"},
-	AllowHeaders:  []string{"*"},
+	AllowHeaders:  []string{"*", "Authorization"},
 	AllowMethods:  []string{"*"},
 	ExposeHeaders: []string{"*"},
 })
