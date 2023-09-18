39c7879cd5e6b1bf95ad5f33254fa707dd102d17
Clement <gh.2lgqz@aleeas.com>
2023-09-18 14:09:30 +0800

    feat: remove code from text before text-to-voice

--- internal/chat.go
+++ internal/chat.go
@@ -120,6 +120,7 @@ func (c *ChatHandler) toSpeech(ctx context.Context, text string, role client.Rol
 
 	go func() { c.sse.PublishData(c.streamId, EventMessageThinking, meta) }()
 
+	text = util.RemoveCodeFromText(text)
 	audio, err := tts.TextToSpeech(ctx, text, *c.o.TTSOption)
 	if err != nil {
 		c.logger.Sugar().Error(err)
--- internal/util/util.go
+++ internal/util/util.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"math/rand"
+	"regexp"
 
 	"github.com/google/uuid"
 )
@@ -31,4 +32,9 @@ func randomString(length int) string {
 func sha256Hash(input string) string {
 	hash := sha256.Sum256([]byte(input))
 	return hex.EncodeToString(hash[:])
+}
+
+func RemoveCodeFromText(text string) string {
+	compile, _ := regexp.Compile("```(.|\\s)*```")
+	return compile.ReplaceAllString(text, "\n")
 }
